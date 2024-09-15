cf0e00e17410bc53d359a73c901ddbd321aeecc6
aaryannagpal <aaryannagpal65@gmail.com>
2024-09-15 15:46:16 +0530

    modified for 64bit, and updated errno library

--- linuxdoom-1.10/i_sound.c
+++ linuxdoom-1.10/i_sound.c
@@ -24,6 +24,8 @@
 static const char
 rcsid[] = "$Id: i_unix.c,v 1.5 1997/02/03 22:45:10 b1 Exp $";
 
+#include<stdint.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -40,6 +42,7 @@ rcsid[] = "$Id: i_unix.c,v 1.5 1997/02/03 22:45:10 b1 Exp $";
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <sys/errno.h>
 
 // Linux voxware output.
 #include <linux/soundcard.h>
@@ -163,7 +166,6 @@ myioctl
   int*	arg )
 {   
     int		rc;
-    extern int	errno;
     
     rc = ioctl(fd, command, arg);  
     if (rc < 0)
